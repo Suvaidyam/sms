ba9361445a365ad41afbff0256423fb01afb069a
vishalkatsa <papajikatsa@gmail.com>
2023-12-25 01:07:09 +0530

    hii pri

--- src/Auth/Register/Register.tsx
+++ src/Auth/Register/Register.tsx
@@ -193,11 +193,13 @@ const Register: FC<RegisterProps> = () => {
                 <div className="relative z-0 w-full mb-5 group">
                   <Field
                     type="number"
+                    min='10'
                     pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                     name="mobile"
                     id="mobile"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
+                    
 
                   />
                   <label
--- src/Pages/Teacher/Teacher.tsx
+++ src/Pages/Teacher/Teacher.tsx
@@ -1,11 +1,59 @@
-import type { FC } from 'react';
+import { useEffect,useState, type FC } from 'react';
+import Http from '../../Services/Http';
 
 interface TeacherProps {}
 
 const Teacher: FC<TeacherProps> = () => {
+    const  [teacherData, setteacherData] = useState<any>([]);
+    console.log(teacherData);
+    
+    useEffect(() => {
+        const fetchdata = async () => {
+            try {
+                let res = await Http({
+                    url: '/auth/seenuser',
+                    method: 'get', 
+                    data: {role:"teacher"} 
+                });
+                const AllTeacher = res.data.user;
+                setteacherData(AllTeacher);
+                const totalTeachersCount = AllTeacher.length;
+                const totalTeachers = sessionStorage.setItem('totalTeachers',totalTeachersCount);
+                
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+        fetchdata();
+    }, []);
     return (
         <>
-            
+                   <>
+        <div className="container mx-auto overflow-x-auto">
+            <table className="w-full min-w-max bg-white shadow-md rounded-lg">
+                <thead className="bg-gray-800 text-white">
+                    <tr>
+                        <th className="py-2 px-4">Username</th>
+                        <th className="py-2 px-4">Email</th>
+                        <th className="py-2 px-4">Role</th>
+                        <th className="py-2 px-4">Mobile</th>
+                        <th className="py-2 px-4">School</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {teacherData.map((teacher: any) => (
+                        <tr key={teacher._id} className="border-t">
+                            <td className="py-2 px-4">{teacher.username}</td>
+                            <td className="py-2 px-4">{teacher.email}</td>
+                            <td className="py-2 px-4">{teacher.role}</td>
+                            <td className="py-2 px-4">{teacher.mobile}</td>
+                            <td className="py-2 px-4">{teacher.school.schoolname}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </>
         </>
     );
 }
